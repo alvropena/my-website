debf01e509b10aa0e6b06c2e6cb998bf9f2032a8
Alvaro Peña <alvropena@Alvaros-Mac-Studio-3.local>
2025-02-03 18:50:26 -0800

    Update social media icons and hover color styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,49 @@
+import { GitHubLogoIcon, TwitterLogoIcon, VideoIcon } from '@radix-ui/react-icons';
+
 export default function Content() {
 	return (
-		<div className="h-screen flex flex-col items-center justify-center gap-4">
-			<h1 className="text-2xl font-light">Alvaro Peña</h1>
-			<ul className="flex gap-4 text-sm text-gray-600">
+		<div className="h-screen flex flex-col items-center justify-center [--hover-color:theme(colors.blue.600)]">
+			<h1 className="text-2xl font-light mb-4">Alvaro Peña</h1>
+			<ul className="flex gap-4 text-sm text-gray-600 mb-4">
 				<li>
-					<a href="/newsletter" className="hover:text-blue-600 group">
+					<a href="https://alvropena.beehiiv.com" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--hover-color)] group">
 						<span className="group-hover:hidden">newsletter</span>
 						<span className="hidden group-hover:inline">[newsletter]</span>
 					</a>
 				</li>
 				<li>
-					<a href="/projects" className="hover:text-blue-600 group">
+					<a href="/projects" className="hover:text-[var(--hover-color)] group">
 						<span className="group-hover:hidden">projects</span>
 						<span className="hidden group-hover:inline">[projects]</span>
 					</a>
 				</li>
 				<li>
-					<a href="/resume" className="hover:text-blue-600 group">
+					<a href="/resume" className="hover:text-[var(--hover-color)] group">
 						<span className="group-hover:hidden">resume</span>
 						<span className="hidden group-hover:inline">[resume]</span>
 					</a>
 				</li>
 				<li>
-					<a href="/contact" className="hover:text-blue-600 group">
+					<a href="/contact" className="hover:text-[var(--hover-color)] group">
 						<span className="group-hover:hidden">contact</span>
 						<span className="hidden group-hover:inline">[contact]</span>
 					</a>
 				</li>
 			</ul>
-			<p className="text-sm text-gray-600 max-w-sm text-center mt-2">
-				Software engineer based in San Francisco, CA.
+			<p className="text-sm text-gray-600 max-w-sm text-center">
+				software engineer based in san francisco, ca.
 			</p>
+			<div className="flex justify-evenly w-full max-w-sm text-gray-600 mt-4">
+				<a href="https://github.com/alvropena" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--hover-color)]">
+					<GitHubLogoIcon className="w-4 h-4" />
+				</a>
+				<a href="https://twitter.com/alvropenaa" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--hover-color)]">
+					<TwitterLogoIcon className="w-4 h-4" />
+				</a>
+				<a href="https://youtube.com/@alvropena" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--hover-color)]">
+					<VideoIcon className="w-4 h-4" />
+				</a>
+			</div>
 		</div>
 	);
 }
